e58218a78192aab9b62c02ac5c5ece5cd64126dc
yukimaru73 <ekxmekxm@gmail.com>
2023-02-24 17:22:38 +0900

    add

--- Tit test.lua
+++ Tit test.lua
@@ -1,19 +1,47 @@
 require("Libs.PhysicsSensorLib")
 require("Libs.Vector3")
 
+--Definition --
+VARS_DEF = {
+	"none", --0
+	"tiltFront", --1
+	"tiltBack", --2
+	"tiltRight", --3
+	"tiltLeft", --4
+	"tiltUp", --5
+	"tiltDown", --6
+	"compass" --7
+}
+
+OUTPUT_VARS = {
+	tiltFront = 0,
+	tiltBack= 0,
+	tiltRight = 0,
+	tiltLeft = 0,
+	tiltUp = 0,
+	tiltDown = 0,
+	compass = 0
+}
+
+-- Properties Definition --
+-- Number --
+TIMELAG = property.getNumber("Time Lag")
+
+
+OUTPUT_CH_DEF = {}
+for i = 1, 32 do
+	OUTPUT_CH_DEF[i] = property.getNumber("Output Channel " .. i)
+end
+-- Boolean --
+
+
+OUTPUT_VARS = {}
 
 PHS = PhisicsSensorLib:new()
 
 VASE_VEC_FRONT = Vector3:new(0, 0, 1)
-VASE_VEC_BACK = Vector3:new(0, 0, -1)
 VASE_VEC_RIGHT = Vector3:new(1, 0, 0)
-VASE_VEC_LEFT = Vector3:new(-1, 0, 0)
 VASE_VEC_UP = Vector3:new(0, 1, 0)
-
-TILT_FRONT = 0
-TILT_LEFT = 0
-TILT_UP = 0
-COMPASS = 0
 
 RAD_TO_TURN = 1 / (2 * math.pi)
 
@@ -25,31 +53,27 @@ function onTick()
 	PHS:update(1)
 
 	-- PROCESS BLOCK --
-	local front = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_FRONT.vec, 0))
-	local left = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_LEFT.vec, 0))
-	local up = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_UP.vec, 0))
+	local front = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_FRONT.vec, TIMELAG))
+	local right = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_RIGHT.vec, TIMELAG))
+	local up = Vector3:newFromArray(PHS:rotateVectorLocal2World(VASE_VEC_UP.vec, TIMELAG))
 
-	TILT_FRONT = front:getElevation() * RAD_TO_TURN
-	TILT_LEFT = left:getElevation() * RAD_TO_TURN
-	TILT_UP = up:getElevation() * RAD_TO_TURN
+	OUTPUT_VARS.tiltFront = front:getElevation() * RAD_TO_TURN
+	OUTPUT_VARS.tiltBack = -OUTPUT_VARS.tiltFront
+	OUTPUT_VARS.tiltRight = right:getElevation() * RAD_TO_TURN
+	OUTPUT_VARS.tiltLeft = -OUTPUT_VARS.tiltRight
+	OUTPUT_VARS.tiltUp = up:getElevation() * RAD_TO_TURN
+	OUTPUT_VARS.tiltDown = -OUTPUT_VARS.tiltUp
+	OUTPUT_VARS.compass = -front:getAzimuth() * RAD_TO_TURN
 
-	COMPASS = -front:getAzimuth() * RAD_TO_TURN
 
-	local altSpeed = 3.6 * (PHS.GPS[3] - ALT_PREV)
 
 	-- OUTPUT BLOCK --
-	output.setNumber(1, PHS.GPS[2])
 
-	output.setNumber(2, PHS.velocity[3])
-	output.setNumber(3, PHS.velocity[2])
-	output.setNumber(4, PHS.velocity[1])
+	for i = 1, 32 do
+		if OUTPUT_CH_DEF[i] ~= 0 then
+			output.setNumber(i, OUTPUT_VARS[VARS_DEF[OUTPUT_CH_DEF[i]]])
+		end
+	end
 
-	output.setNumber(5, TILT_LEFT)
-	output.setNumber(6, TILT_FRONT)
-	output.setNumber(7, TILT_UP)
-	output.setNumber(8, COMPASS)
-	output.setNumber(9, altSpeed)
-
-	ALT_PREV = PHS.GPS[3]
 end
 
